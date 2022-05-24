6e458afb1e2454538fe09d76a51957533760bd38
JacobKosowski <99693535+JacobKosowski@users.noreply.github.com>
2022-05-24 11:02:28 +0300

    Update myosotis.py
    Moved some blocks of code to dedicated functions.py
    Isolated the log output to be consolidated into one function
    Check for FOV stars prior to main loop in order to determine final size of arrays
    Option added to skip isochrone matching if Lum, radius, and teff are provided by simulation. In prep for adding the handeling of this kind of data

--- myosotis.py
+++ myosotis.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
-import sys
 from math import *
 import random
 import numpy as np
-from scipy.linalg import expm
-from astropy.io import fits
-import os, sys, shutil
 import timeit
 import params_clean as params
 import functions
@@ -19,12 +15,6 @@ start = timeit.default_timer()
 functions.myso_logo('logo')
 
 # Reads in parameters
-
-project_name=params.project_name
-EXTmodel=params.EXTmodel
-filterfile=params.filterfile
-filestar=params.filestar
-filecloud=params.filecloud
 Columndensities=params.Columndensities
 OBtreatment=params.OBtreatment
 res=params.res
@@ -49,23 +39,11 @@ Adaptiveoptics=params.Adaptiveoptics
 seeing=params.seeing
 SR=params.SR
 
-Mbolsun = 4.77
-
-#######################################################################################################################
-# Assigns output
-
-outputim     = project_name+'/'+project_name+'_image.fits' 
-outputimnoise=project_name+'/'+project_name+'_imageNoise.fits'
-outputspecFL = project_name+'/'+project_name+'_cube_spectra.fits'
-outputspecL  = project_name+'/'+project_name+'_Lambda.txt'
-outputstarinfo  = project_name+'/'+project_name+'_star_info.txt'
-
-
 #######################################################################################################################
 # Interstellar dust models
 
 DrainearrLam, DrainearrK=[0.0],[0.0]
-if (EXTmodel == 'Dmodel'):
+if (params.EXTmodel == 'Dmodel'):
     DrainearrLamu,drainealbedo,drainecos,draineC,DrainearrKu,drainecos2=np.loadtxt(directories.Drainemodel,usecols=(0,1,2,3,4,5),unpack=True)
     DrainearrLamu = DrainearrLamu*1.0E+4
     DrainearrKu = DrainearrKu/constants.DraineKappaV
@@ -74,7 +52,7 @@ if (EXTmodel == 'Dmodel'):
 #######################################################################################################################
 # 
 
-lambdaF,weight=np.loadtxt(filterfile,unpack=True)
+lambdaF,weight=np.loadtxt(params.filterfile,unpack=True)
 
 xpix=round(fovx/res)
 ypix=round(fovy/res)
@@ -101,7 +79,7 @@ if (spectroscopy == 'yes'):
     nspec=len(Lspecarr)
     sceneL=np.zeros(nspec)
     sceneimFL=np.full((nspec,int(ypix),int(xpix)),0.0)
-    lun=open(outputspecL,"w")
+    lun=open(directories.outputspecL,"w")
     lun.write('# Lambda[A] \n')
     for ll in range(nspec): lun.write("%f\n" %(Lspecarr[ll]))  
     lun.close()
@@ -132,28 +110,11 @@ if (OBtreatment == 'yes'):
 
 #wavelength,flux=np.loadtxt(foldersed+sedname[10],comments=['fn:', '#'],unpack=True)
 
-#######################################################################################################################
-# Structures output files
-
-if not os.path.exists(project_name):
-    os.makedirs(project_name)
-else:
-    shutil.rmtree(project_name)           
-    os.makedirs(project_name)
-
-
-lunstarinfo=open(outputstarinfo,"w")
-
-lunstarinfo.write("#     mass[mo] ,   logage[yr]  ,     Z      , log[Teff[k]] ,    logg    ,   logL/Lo    ,  Av_star   ,      mag     ,    Xpix     ,   Ypix       ,       assignedSED \n")
-
-lunstarinfo.write("#       1              2             3            4              5              6           7               8            9            10                   11 \n")
-
-fovstars_check = np.full(nstar,False)
 nfovstars=0
 for ii in range(nstar):
     if ((abs(newx[ii]) < (xpix/2)-1) and (abs(newy[ii]) < (ypix/2)-1)): # Check if star is within chosen FOV
-        fovstars_check[ii] = True
-
+        nfovstars += 1
+    else:
         np.delete(logagestar,[ii])
         np.delete(massstar,[ii])
         np.delete(kzstar,[ii])
@@ -162,15 +123,16 @@ for ii in range(nstar):
         np.delete(newz,[ii])
         np.delete(pc2pixstar,[ii])
 
-
-        nfovstars += 1
-
 Teffstar=np.zeros(nfovstars)
 loggstar=np.zeros(nfovstars)
 loglstar=np.zeros(nfovstars)
 sedstar=np.zeros(nfovstars,dtype=np.uint64)
 fluxstar=np.zeros(nfovstars)
 columncloud=np.zeros(nfovstars) #column density of the cloud in front of each star
+AVstar=np.zeros(nfovstars)
+mag=np.zeros(nfovstars)
+readsed=np.full(nfovstars,None)
+
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 # Add parallelization here
 # Each star is fully independent so this loop can be split as many times as needed
@@ -187,13 +149,13 @@ for ii in range(nfovstars):
         nstars += 1
 
     # Density of gas clouds
-    if (Columndensities == 'sph'):
+    if (params.Columndensities == 'sph'):
         columncloud[ii]=functions.COLUMN_DENSITY(newx[ii],newy[ii],newz[ii],newxcloud,newycloud,newzcloud,masspar,newhcloud)
         cloudden=columncloud[ii]*((1.989/1.673534)/(((3.0857/pc2pixstar[ii])**2.))) #*1.0e21 ;convert column density unit [Msun/pix^2] --> [10^21 * Mhydrogen/cm^2]
     else: cloudden=rhostar[ii]*((1.989/1.673534)/((3.0857**2.)))#*1.0e21 ;convert column density unit [Msun/pc^2] --> [10^21 * Mhydrogen/cm^2]
 
     #        cloudden=rhostar[ii]*((1.989/1.673534)/((3.0857**2))) #*1.0e21 #convert column density unit [Msun/pc^2] --> [10^21 * Mhydrogen/cm^2]
-    AVstar=cloudden/2.21 #e21 ;Guver&Ozel2009: The relation between Optical Extinction and Hydrogen column density
+    AVstar[ii]=cloudden/2.21 #e21 ;Guver&Ozel2009: The relation between Optical Extinction and Hydrogen column density
     
     # If lum, teff, and radius are not given, use isochrones to calculate appropriate values
     if not params.LTRprovided:
@@ -215,7 +177,9 @@ for ii in range(nfovstars):
         if (deltaT[jj] == min(deltaT)):  deltagarr[jj]=abs(loggstar[ii]-loggsed[jj])
 #        sedstar[ii]=where(deltagarr eq min(deltagarr))
     sedstar[ii]=functions.FINDCLOSE(min(deltagarr),deltagarr)
-    readsed=sedname[sedstar[ii]]
+
+    readsed[ii]=sedname[sedstar[ii]].decode('UTF-8')
+
 #        readcol,foldersed+sedname[sedstar[ii]],wavelength,flux,/silent
     wavelength,flux=np.loadtxt(directories.foldersed+sedname[sedstar[ii]].decode('UTF-8'),comments=['fn:', '#'],unpack=True)
 
@@ -227,16 +191,15 @@ for ii in range(nfovstars):
 #            sedstar[ii]=where(deltagarr eq min(deltagarr))
         sedstar[ii]=functions.FINDCLOSE(min(deltagarr),deltagarr)
 
-        readsed=sednameOB[sedstar[ii]]
+        readsed[ii]=sednameOB[sedstar[ii]].decode('UTF-8')
 #            readcol,foldersed+sednameOB[sedstar[ii]],wavelength,flux,/silent
         wavelength,flux=np.loadtxt(directories.foldersed+sednameOB[sedstar[ii]].decode('UTF-8'),comments=['fn:', '#'],unpack=True)
 
 
-    bc1=functions.BCcal(wavelength,flux,lambdaF,weight,AVstar,Rv,Teffstar[ii],par2vega,EXTmodel,DrainearrLam,DrainearrK)
-    mag=Mbolsun-2.5*loglstar[ii]-(bc1)+5.0*log10(distancestar[ii]/10.0)
-    fluxstar[ii]=10.**(mag/(-2.5))
+    bc1=functions.BCcal(wavelength,flux,lambdaF,weight,AVstar[ii],Rv,Teffstar[ii],par2vega,params.EXTmodel,DrainearrLam,DrainearrK)
+    mag[ii]=constants.Mbolsun-2.5*loglstar[ii]-(bc1)+5.0*log10(distancestar[ii]/10.0)
+    fluxstar[ii]=10.**(mag[ii]/(-2.5))
 
-    lunstarinfo.write("%13.4f %13.4f %13.4f %13.4f %13.4f %13.4f %13.4f %13.4f %13.4f %13.4f %60s \n" %(massstar[ii], logagestar[ii],kzstar[ii],log10(Teffstar[ii]),loggstar[ii],loglstar[ii],AVstar,mag,newx[ii]+xpix/2.,newy[ii]+ypix/2.,readsed.decode('UTF-8')))
 
     if (PSFtype == 'gaussian'):
         airy1=functions.makeGaussian((xpix,ypix),fwhm/res,center=(newx[ii]+xpix/2.,newy[ii]+ypix/2.))
@@ -254,8 +217,8 @@ for ii in range(nfovstars):
         for ll in range(2,nspec-3): 
 #                linterp,lambda,weight,wavelength,wavelength_weight
             wavelength_weight=np.interp(wavelength,lambdaF,weight)
-            bc3=functions.BCcals(wavelength,flux*wavelength_weight,[Lspecarr[ll-1],Lspecarr[ll],Lspecarr[ll+1],Lspecarr[ll+2]],[0.0,1.0,1.0,0.0],AVstar,Rv,Teffstar[ii],EXTmodel,DrainearrLam,DrainearrK)
-            mag3=Mbolsun-2.5*loglstar[ii]-bc3+5.0*log10(distancestar[ii]/10.0)
+            bc3=functions.BCcals(wavelength,flux*wavelength_weight,[Lspecarr[ll-1],Lspecarr[ll],Lspecarr[ll+1],Lspecarr[ll+2]],[0.0,1.0,1.0,0.0],AVstar[ii],Rv,Teffstar[ii],params.EXTmodel,DrainearrLam,DrainearrK)
+            mag3=constants.Mbolsun-2.5*loglstar[ii]-bc3+5.0*log10(distancestar[ii]/10.0)
             fluxstar3=float(10.**(mag3/(-2.5)))
             if (PSFtype == 'gaussian'):
                 airy3=functions.makeGaussian((xpix,ypix),fwhm/res,center=(newx[ii]+xpix/2.,newy[ii]+ypix/2.))
@@ -279,52 +242,21 @@ for ii in range(nfovstars):
 print('Number of valid stars in the FoV: ',nstars)
 
 
-hdu = fits.PrimaryHDU(sceneim)
-hdu.writeto(outputim)
+faintestflux, noise2addim, noise = functions.noise_for_image(flux,xpix,ypix)
 
-faintestflux=min(i for i in fluxstar if i > 0)
-if (SNR != 0.0): 
-    noise=faintestflux*4.*log(2.0)/(pi*(fwhm/res)*(fwhm/res))/SNR 
-else: 
-    noise=noise2add
+#######################################################################################################################
+# Create output logs+images and print output filenames and execution time
 
-noise2addim=noise*np.random.rand(int(ypix),int(xpix))
+functions.create_image(sceneim,noise2addim,sceneimFL)
 
-hdu = fits.PrimaryHDU(noise2addim)
-hdu.writeto(outputimnoise)
-
-if (spectroscopy == 'yes'):
-    hdu = fits.PrimaryHDU(sceneimFL)
-    hdu.writeto(outputspecFL)
-
-lunstarinfo.write('#faintestflux: %f \n' %(faintestflux))
-lunstarinfo.write('#noise:  %f \n' %(noise))
-lunstarinfo.write('#SNR:  %f \n' %(SNR))
-lunstarinfo.write('#Filter:  %s \n' %(filterfile))
-lunstarinfo.write('#distance:  %f \n' %(distance))
-lunstarinfo.write('#res:  %f \n' %(res))
-lunstarinfo.write('#FoV:  %d %d \n' %(fovx,fovy))
-lunstarinfo.write('#fwhm:  %f \n' %(fwhm))
-lunstarinfo.write('#OBtreatment:  %s \n' %(OBtreatment))
-lunstarinfo.write('#Adaptiveoptics:  %s \n' %(Adaptiveoptics))
-lunstarinfo.write('#SR:  %f \n' %(SR))
-lunstarinfo.write('#Seeing:  %f \n' %(seeing))
-lunstarinfo.write('#Spectroscopic Resolution:  %f \n' %(Rspec))
-lunstarinfo.write('#Lambda_min:  %f \n' %(lminspec))
-lunstarinfo.write('#Lambda_max:  %f \n' %(lmaxspec))
-lunstarinfo.write('#filestar:  %s \n' %(filestar))
-lunstarinfo.write('#Columndensities:  %s \n' %(Columndensities))
-lunstarinfo.write('#filecloud:  %s \n' %(filecloud))
-lunstarinfo.write('#EXTmodel:  %s \n' %(EXTmodel))
-lunstarinfo.write('#Rv:  %f \n' %(Rv))
-lunstarinfo.close()
+functions.log_output(noise,faintestflux,massstar,logagestar,kzstar,Teffstar,loggstar,loglstar,AVstar,mag,newx+xpix/2.0,newy+ypix/2.0,readsed,directories.outputstarinfo)
 
 functions.myso_logo('outim')
-print(outputim)
+print(directories.outputim)
 print('   ')
 functions.myso_logo('outspec')
-print(outputspecFL)
-print(outputspecL)
+print(directories.outputspecFL)
+print(directories.outputspecL)
 print('   ')
 stop = timeit.default_timer()
 

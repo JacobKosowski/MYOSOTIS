9afd5ca952b2c770f7599c500a5d6e571d7a6b60
JacobKosowski <99693535+JacobKosowski@users.noreply.github.com>
2022-06-06 16:12:31 +0300

    Add files via upload

--- directories.py
+++ directories.py
@@ -26,15 +26,14 @@ sed_vega='SEDs/vegaf'
 sed_z1=foldersed+'Z1/'
 sed_z0p5=foldersed+'Z0p5/'
 
-
-
 #Isochrones
 iso_z0p015='Evolutionary/Z0p015.dat'
 iso_z0p008='Evolutionary/Z0p008.dat'
 
 if (params.metallicityZ == 1.0):
     isochrones=iso_z0p015
-    foldersed=sed_z1
+    foldersed=sed_z1+'HDF/'
+    # foldersed=sed_z1+'CSV/'
 elif (params.metallicityZ == 0.5):
     isochrones=iso_z0p008
     foldersed=sed_z0p5

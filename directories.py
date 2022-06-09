0fc001b2347a51847e0e55dce12080baad4799fe
JacobKosowski <99693535+JacobKosowski@users.noreply.github.com>
2022-06-09 12:29:41 +0300

    Add files via upload

--- directories.py
+++ directories.py
@@ -1,5 +1,6 @@
 import params_clean as params
 import os, shutil
+import functions
 
 #######################################################################################################################
 # Structures output files
@@ -25,6 +26,7 @@ foldersed='SEDs/'
 sed_vega='SEDs/vegaf'
 sed_z1=foldersed+'Z1/'
 sed_z0p5=foldersed+'Z0p5/'
+mergedseds='merged.hdf'
 
 #Isochrones
 iso_z0p015='Evolutionary/Z0p015.dat'
@@ -32,6 +34,7 @@ iso_z0p008='Evolutionary/Z0p008.dat'
 
 if (params.metallicityZ == 1.0):
     isochrones=iso_z0p015
+    # foldersed=sed_z1+'FEA/'
     foldersed=sed_z1+'HDF/'
     # foldersed=sed_z1+'CSV/'
 elif (params.metallicityZ == 0.5):
@@ -54,3 +57,14 @@ elif (params.Rv == 5.5):
     Drainemodel=draine5p5
 else: print('For Dmodel, R_V should be 3.1 or 4.0 or 5.5. If you need other Rv values please choose Fmodel')
 
+def display():
+
+    functions.myso_logo('outim')
+    print(outputim)
+    print('   ')
+
+    if params.spectroscopy=='yes':
+        functions.myso_logo('outspec')
+        print(outputspecFL)
+        print(outputspecL)
+        print('   ')
